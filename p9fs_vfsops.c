ea4aa7994570c82ec990c914d1f2639ccfb72338
Will Andrews <will@firepipe.net>
2015-06-24 17:38:07 -0600

    Initial mount/unmount implementations.
    While working on this, I realized that a mount_p9fs is necessary, so
    checking this in compile-tested so this can be reorganized.

--- p9fs_vfsops.c
+++ p9fs_vfsops.c
@@ -31,19 +31,253 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
+#include <sys/types.h>
+#include <sys/systm.h>
 #include <sys/mount.h>
 #include <sys/kernel.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/mbuf.h>
+#include <sys/malloc.h>
+#include <sys/mutex.h>
+#include <sys/protosw.h>
+#include <sys/sockopt.h>
+#include <sys/socketvar.h>
+#include <sys/pcpu.h>
+#include <sys/proc.h>
+#include <sys/vnode.h>
+
+static const char *p9_opts[] = {
+	"debug",
+};
+
+struct p9fsmount {
+	struct sockaddr p9_sockaddr;
+	int p9_sockaddr_len;
+	int p9_socktype;
+	int p9_proto;
+	int p9_debuglevel;
+	struct socket *p9_socket;
+	struct mount *p9_mountp;
+};
+#define	VFSTOP9(mp) ((mp)->mnt_data)
+
+static MALLOC_DEFINE(M_P9MNT, "p9fsmount", "Mount structures for p9fs");
 
 static int
-p9fs_mount(struct mount *mp)
+p9fs_mount_parse_opts(struct mount *mp)
 {
-	return (EINVAL);
+	struct p9fsmount *p9mp = VFSTOP9(mp);
+	char *opt;
+	int error = EINVAL;
+	int ret;
+
+	if (vfs_getopt(mp->mnt_optnew, "debug", (void **)&opt, NULL) == 0) {
+		if (opt == NULL) {
+			vfs_mount_error(mp, "illegal debug");
+			goto out;
+		}
+		ret = sscanf(opt, "%d", &p9mp->p9_debuglevel);
+		if (ret != 1 || p9mp->p9_debuglevel < 0) {
+			vfs_mount_error(mp, "illegal debug: %s", opt);
+			goto out;
+		}
+	}
+
+	/* Flags beyond here are not supported for updates. */
+	if (mp->mnt_flag & MNT_UPDATE)
+		return (0);
+
+	ret = vfs_getopt(mp->mnt_optnew, "addr",
+	    (void **)&p9mp->p9_sockaddr, &p9mp->p9_sockaddr_len);
+	if (ret == 0) {
+		if (p9mp->p9_sockaddr_len > SOCK_MAXADDRLEN) {
+			error = ENAMETOOLONG;
+			goto out;
+		}
+	} else {
+		vfs_mount_error(mp, "No server address");
+		goto out;
+	}
+
+	if (vfs_getopt(mp->mnt_optnew, "proto", (void **)&opt, NULL) == 0) {
+		if (strcasecmp(opt, "tcp") == 0) {
+			p9mp->p9_socktype = SOCK_STREAM;
+			p9mp->p9_proto = IPPROTO_TCP;
+		} else if (strcasecmp(opt, "udp") == 0) {
+			p9mp->p9_socktype = SOCK_DGRAM;
+			p9mp->p9_proto = IPPROTO_UDP;
+		} else {
+			vfs_mount_error(mp, "illegal proto: %s", opt);
+			goto out;
+		}
+	}
+
+	error = 0;
+
+out:
+	return (error);
+}
+
+static void
+p9fs_setsockopt(struct socket *so, int sopt_name)
+{
+	struct sockopt sopt = { 0 };
+	int one = 1;
+
+	sopt.sopt_dir = SOPT_SET;
+	sopt.sopt_level = SOL_SOCKET;
+	sopt.sopt_name = sopt_name;
+	sopt.sopt_val = &one;
+	sopt.sopt_valsize = sizeof(one);
+	sosetopt(so, &sopt);
+}
+
+/*
+ * XXX Need to implement reconnecting as necessary.  If that were to be
+ *     needed, most likely all current vnodes would have to be renegotiated
+ *     or otherwise invalidated (a la NFS "stale file handle").
+ */
+static int
+p9fs_connect(struct mount *mp)
+{
+	struct p9fsmount *p9mp = VFSTOP9(mp);
+	struct socket *so;
+	int error;
+
+	error = socreate(p9mp->p9_sockaddr.sa_family, &p9mp->p9_socket,
+	    p9mp->p9_socktype, p9mp->p9_proto, curthread->td_ucred, curthread);
+	if (error != 0)
+		goto out;
+
+	so = p9mp->p9_socket;
+	error = soconnect(so, &p9mp->p9_sockaddr, curthread);
+	SOCK_LOCK(so);
+	while ((so->so_state & SS_ISCONNECTING) && so->so_error == 0) {
+		error = msleep(&so->so_timeo, SOCK_MTX(so), PSOCK | PCATCH,
+		    "connec", 0);
+		if (error)
+			break;
+	}
+	if (error == 0) {
+		error = so->so_error;
+		so->so_error = 0;
+	}
+	SOCK_UNLOCK(so);
+	if (error) {
+		if (error == EINTR)
+			so->so_state &= ~SS_ISCONNECTING;
+		goto out;
+	}
+
+	if (so->so_proto->pr_flags & PR_CONNREQUIRED)
+		p9fs_setsockopt(so, SO_KEEPALIVE);
+	if (so->so_proto->pr_protocol == IPPROTO_TCP)
+		p9fs_setsockopt(so, TCP_NODELAY);
+
+	/* XXX Do we need this?  Probably?  See clnt_vc_soupcall(). */
+#if 0
+	SOCKBUF_LOCK(&so->so_rcv);
+	soupcall_set(so, SO_RCV, p9fs_sock_upcall, p9mp);
+	SOCKBUF_UNLOCK(&so->so_rcv);
+#endif
+
+	error = 0;
+
+out:
+	return (error);
+}
+
+static int
+p9fs_negotiate(struct p9fsmount *p9mp)
+{
+	int error = EINVAL;
+
+	/* Call into p9fs_client_version/auth/attach */
+
+	return (error);
+}
+
+static void
+p9fs_mount_alloc(struct p9fsmount **p9mpp, struct mount *mp)
+{
+	*p9mpp = malloc(sizeof (struct p9fsmount), M_P9MNT, M_WAITOK | M_ZERO);
+	mp->mnt_data = *p9mpp;
+	(*p9mpp)->p9_mountp = mp;
+
+	/* Default values. */
+	(*p9mpp)->p9_socktype = SOCK_STREAM;
+	(*p9mpp)->p9_proto = IPPROTO_TCP;
 }
 
 static int
 p9fs_unmount(struct mount *mp, int mntflags)
 {
-	return (EINVAL);
+	struct p9fsmount *p9mp = VFSTOP9(mp);
+	int error, flags, i;
+
+	error = 0;
+	flags = 0;
+	if (p9mp == NULL)
+		return (0);
+
+	if (mntflags & MNT_FORCE)
+		flags |= FORCECLOSE;
+
+	for (i = 0; i < 10; i++) {
+		error = vflush(mp, 0, flags, curthread);
+		if (error != 0 && (mntflags & MNT_FORCE) == 0)
+			break;
+		/* Sleep until interrupted or 1 second expires. */
+		error = tsleep(&error, PSOCK, "p9unmnt", 1);
+		if (error == EINTR)
+			break;
+	}
+	if (error != 0)
+		goto out;
+
+	if (p9mp->p9_socket != NULL)
+		(void) soclose(p9mp->p9_socket);
+
+	free(p9mp, M_P9MNT);
+	mp->mnt_data = NULL;
+
+out:
+	return (error);
+}
+
+static int
+p9fs_mount(struct mount *mp)
+{
+	struct p9fsmount *p9mp;
+	int error;
+
+	error = EINVAL;
+	if (vfs_filteropt(mp->mnt_optnew, p9_opts))
+		goto out;
+
+	if (mp->mnt_flag & MNT_UPDATE)
+		return (p9fs_mount_parse_opts(mp));
+
+	p9fs_mount_alloc(&p9mp, mp);
+
+	error = p9fs_mount_parse_opts(mp);
+	if (error != 0)
+		goto out;
+
+	error = p9fs_connect(mp);
+	if (error != 0)
+		goto out;
+
+	error = p9fs_negotiate(p9mp);
+	if (error != 0)
+		goto out;
+
+out:
+	if (error != 0)
+		p9fs_unmount(mp, 0);
+	return (error);
 }
 
 static int
